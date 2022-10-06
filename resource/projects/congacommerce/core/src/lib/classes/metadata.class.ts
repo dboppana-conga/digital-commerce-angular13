57731650dc7a62d9be9179a8bc49eafb8e9a22ef
divinabopanna <dboppana@apttus.com>
2022-10-06 10:21:56 +0530

    updated metadata

--- resource/projects/congacommerce/core/src/lib/classes/metadata.class.ts
+++ resource/projects/congacommerce/core/src/lib/classes/metadata.class.ts
@@ -1,22 +1,22 @@
 export class Metadata{
-    Id: string = null;
-    Name: string = null;
-    Label: string = null;
-    Description: string = null;
+    Id: string | null = null;
+    Name: string | null = null;
+    Label: string | null = null;
+    Description: string | null = null;
     Fields: Array<MetadataField> = [new MetadataField()];
 }
 
 export class MetadataField{
-    type: string = null;
-    label: string = null;
+    type: string | null = null;
+    label: string | null = null;
     length: number = 0;
     defaultValue: any;
-    name: string;
+    name: string | null = null;
     picklistValues: Array<Picklist> = [new Picklist()];
 }
 
 export class Picklist{
-    label: string = null;
-    value: string = null;
+    label: string | null = null;
+    value: string | null = null;
 }
 
