793e63a773136ca117bfe08432017acd498a1e23
APTTUS\vbhan <vbhan@conga.com>
2022-10-12 21:16:21 +0530

    Fixed error on favorite service

--- resource/projects/congacommerce/ecommerce/src/lib/modules/favorite/services/favorite.service.ts
+++ resource/projects/congacommerce/ecommerce/src/lib/modules/favorite/services/favorite.service.ts
@@ -17,7 +17,7 @@ import { FavoriteItemRequest } from '../interfaces/index';
   providedIn: 'root'
 })
 export class FavoriteService extends AObjectService {
-  type = Favorite;
+  override type = Favorite;
 
   protected cartService: CartService = this.injector.get(CartService);
   protected priceListService: PriceListService = this.injector.get(PriceListService);
@@ -39,7 +39,7 @@ export class FavoriteService extends AObjectService {
    * @param lookups string value representing the lookups to be fetched for a given favorite.
    * @returns observable containing the favorite record.
    */
-  getFavoriteById(favoriteId: string, lookups: string = null): Observable<Favorite> {
+  getFavoriteById(favoriteId: string, lookups: string | null = null): Observable<Favorite> {
     const queryParam = lookups ? `?lookups=${lookups}` : '';
     return this.apiService.get(`/Apttus_Config2__FavoriteConfiguration__c/${favoriteId}${queryParam}`, this.type);
   }
@@ -101,7 +101,7 @@ export class FavoriteService extends AObjectService {
    */
   addFavoriteToCart(favorite: string | Favorite): Observable<Array<CartItem>> {
     const favoriteId = favorite instanceof Favorite ? favorite.Id : favorite;
-    return this.cartService.addItem({ 'FavoriteId': favoriteId });
+    return this.cartService.addItem({ 'FavoriteId': favoriteId as string });
   }
 
   /**
@@ -130,7 +130,7 @@ export class FavoriteService extends AObjectService {
     const requestObj = map(payload, (item) => item.strip());
     return this.apiService
       .post(`/favorites`, requestObj, this.type)
-      .pipe(rmap(first));
+      .pipe(rmap(res => first(res) as Favorite));
   }
 
   /**
@@ -166,7 +166,7 @@ export class FavoriteService extends AObjectService {
     const favorites = filter(favoriteList, f => !isNil(f));
     const payload = every(favorites, fav => typeof (fav) === 'string') ?
       map(favorites, fav => ({ Id: fav }))
-      : map(favorites, fav => ({ Id: fav.Id }));
+      : map(favorites, fav => ({ Id: get(fav, 'Id') }));
     return this.apiService.post(`/favorites/delete`, payload, this.type);
   }
 }
