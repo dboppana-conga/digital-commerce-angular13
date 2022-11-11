c5ccf1394ce2c6ea989bce7289ca1cca60294d9e
divinabopanna <dboppana@apttus.com>
2022-11-11 11:08:13 +0530

    resolved core error

--- resource/projects/congacommerce/core/src/lib/telemetry/telemetry.module.ts
+++ resource/projects/congacommerce/core/src/lib/telemetry/telemetry.module.ts
@@ -3,34 +3,34 @@ import { CommonModule } from '@angular/common';
 import { get } from 'lodash';
 import { ConfigurationService } from '../services/configuration.service';
 
-const panoptos = get(window, 'panoptos');
+const panoptos = get(window, 'panoptos') || null;
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule]
 })
-
-
 export class TelemetryModule {
   constructor(configService: ConfigurationService) {
-    panoptos.panoptos.init({
-      service: {
-        serviceName: 'dc',
-        ServiceNamespace: 'frontEnd'
-      },
-      userId: localStorage.getItem('userId'),
-      clientId: configService.get('clientId'),
-      enableLogging: true,
-      enableMetrics: false,
-      enableTracing: true,
-      propagateTraceHeaderCorsUrls: [/rlp-dev.congacloud.io/, /rlp-qa.congacloud.io/, /rlp-stg.congacloud.io/],
-      collectorUrl: 'https://signals.prod-sodium.conga-panoptos.com',
-      console: false,
-      ignoreUrls: [/signals.prod-sodium.conga-panoptos.com/],
-      useBeacon: false,
-      instrumentations: panoptos.defaultInstrumentations,
-      providers: panoptos.defaultProviders
-    });
+     panoptos.panoptos.init({
+        service: {
+          serviceName: 'dc',
+          ServiceNamespace: 'frontEnd',
+        },
+        userId: localStorage.getItem('userId'),
+        clientId: configService.get('clientId'),
+        enableLogging: true,
+        enableMetrics: false,
+        enableTracing: true,
+        propagateTraceHeaderCorsUrls: [
+          /rlp-dev.congacloud.io/,
+          /rlp-qa.congacloud.io/,
+          /rlp-stg.congacloud.io/,
+        ],
+        collectorUrl: 'https://signals.prod-sodium.conga-panoptos.com',
+        console: false,
+        ignoreUrls: [/signals.prod-sodium.conga-panoptos.com/],
+        useBeacon: false,
+        instrumentations: get(panoptos, 'defaultInstrumentations'),
+        providers: get(panoptos, 'defaultProviders'),
+      });
   }
 }
